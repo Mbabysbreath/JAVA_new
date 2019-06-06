88ba7e2c65f69f7895d077cf25440c1f4f9e96a0
Mbabysbreath <1830817543@qq.com>
2019-06-07 00:16:22 +0800

    乘法表

---
+++ MulTab.java
@@ -0,0 +1,16 @@
+public class MulTab{
+	public static void main(String[] args){
+		int i=0;
+		int j=0;
+		int sum=1;
+		for(i=1;i<10;i++){
+			for(j=1;j<=i;j++){
+				sum=i*j;
+				System.out.print(" "+i+"*"+j+"="+sum);
+				System.out.printf("%d * %d =%d\t",i,j,sum）;
+			}
+			System.out.println();
+		}
+		
+	}
+}
