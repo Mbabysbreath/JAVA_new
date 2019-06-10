ddd5ee7769d28625bfe4a5d4666fabb395ef8a11
Mbabysbreath <1830817543@qq.com>
2019-06-10 23:39:35 +0800

    代码

---
+++ Partition.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Partition {
+    public ListNode partition(ListNode pHead, int x) {
+        // write code here
+    }
+}
