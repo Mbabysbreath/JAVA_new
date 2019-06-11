385cc994f209e8fae506bee6efa490c177af2e7e
Mbabysbreath <48411220+Mbabysbreath@users.noreply.github.com>
2019-06-11 23:19:05 +0800

    分割链表

--- Partition.java
+++ Partition.java
@@ -1,16 +1,58 @@
-import java.util.*;
-
-/*
-public class ListNode {
-    int val;
-    ListNode next = null;
-
-    ListNode(int val) {
-        this.val = val;
-    }
-}*/
-public class Partition {
-    public ListNode partition(ListNode pHead, int x) {
+public static ListNode partition(ListNode pHead, int x) {
         // write code here
-    }
+        ListNode sList= null;
+        ListNode lList=null;
+        ListNode last2=null;
+        ListNode last1=null;
+		ListNode cur=pHead;
+       // ListNode lHead=new ListNode(x);
+        while(cur!=null){
+            ListNode next=cur.next;
+            if(cur.val<=x){
+                if(last1==null){
+                    sList=cur;
+                    last1=cur;
+                }
+                else{
+                    last1.next=cur;
+                    last1=cur;
+                }
+                
+            }
+            
+            else{
+                if(last2==null){
+                    lList=cur;
+                    last2=cur;
+                }
+				else{
+                 last2.next=cur;
+                 last2=cur;
+                
+            }
+            cur=next;
+        }   
+	}
+		last1.next=lList;
+        return sList;
+ }
+	public static void print(ListNode head){
+		ListNode cur=head;
+		while(cur!=null){
+			System.out.printf("%d-->",cur.val);
+			cur=cur.next;
+		}
+			System.out.println("null");
+	}
+	public static void main(String args[]){
+		ListNode n1=new ListNode(1);
+		ListNode n2=new ListNode(6);
+		ListNode n3=new ListNode(2);
+        ListNode n4=new ListNode(3);
+		n1.next=n2;n2.next=n3;
+		n3.next=n4;
+		print(n1);
+		ListNode m=partition(n1,1);
+		print(m);
+	}
 }
