b31818a974a37efbcf1ddfa5bd28b0723f767b07
Mbabysbreath <1830817543@qq.com>
2019-12-21 22:59:13 +0800

    GUI

---
+++ GUI20191221/FrameTest.java
@@ -0,0 +1,16 @@
+package GUI20191221;
+
+import java.awt.*;
+
+/**
+ * @author ZhaoMin
+ * @date 2019/12/21 22:56
+ */
+public class FrameTest {
+    public static void main(String[] args) {
+        Frame f=new Frame("Frame");
+        f.setBounds(400,300,400,300);
+       // f.addWindowListener();
+        f.setVisible(true);
+    }
+}
